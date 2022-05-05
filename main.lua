9cead94be94a317ae735c3b521bf999b6a5bf4dc
Ap0c-dev <tiago.barros@estudante.iftm.edu.br>
2022-05-04 23:10:14 -0300

    Correções feitas

--- main.lua
+++ main.lua
@@ -3,54 +3,36 @@ display.setStatusBar(display.HiddenStatusBar);
 _w = display.contentWidth;
 _h = display.contentHeight;
 
+--criar um grupo para que consiga posicionar o target atrás do circulo
+local myGroup = display.newGroup()
 
-local object = display.newImage("target_white.png" )
-object.id = "ball object"
+--image rect para que possa redimensionar
+local object = display.newImageRect("target_white.png", 60,60)
+
 object.x = _w/2
 object.y = _h/2
-direcao = 0
- 
+
 function object:touch( event )
     if ( event.phase == "began" ) then        
-        print( "Touch event began on: " .. self.id )
- 
-        -- Set touch focus
         display.getCurrentStage():setFocus( self )
         self.isFocus = true
  
     elseif ( self.isFocus ) then
         if ( event.phase == "moved" ) then
-            print( "Moved phase of touch event detected." )
             object.x = event.x
             object.y = event.y
             
         elseif ( event.phase == "ended" or event.phase == "cancelled" ) then
- 
-            -- Reset touch focus
-           object = display.getCurrentStage():setFocus( nil )
-            self.isFocus = nil
-           
+            object.x =  _w/2
+            object.y = _h/2          
         end
     end
     return true
 end
 Runtime:addEventListener( "touch", object )
 
--- local function mover(event)
---     object.x = event.x
---     object.y = event.y
--- end
--- Runtime:addEventListener( "touch", mover )
+local target = display.newCircle(myGroup, _w/2, _h/2,  40 )
+target:setFillColor( 0)
+target.strokeWidth = 5
+target:setStrokeColor( 1, 0, 0 )
 
-
--- Runtime:addEventListener("touch", move)
--- local function touchTela(ev)
---     if ev.phase == "began" then
---         if ev.x >= _w/2 then
---             object.direcao = 1;
---         else
---             object.direcao = -1;
---         end
---     elseif ev.phase == "ended" then
---         object.direcao = 0;
---end
